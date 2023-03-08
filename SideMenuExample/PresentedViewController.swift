79e94055562c1c3f050806c0c83874f24aff4368
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-08 11:44:07 +0100

    Initial example

---
+++ SideMenuExample/PresentedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PresentedViewController.swift
+//  SideMenuExample
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 08.03.23.
+//
+
+import UIKit
+
+class PresentedViewController: UIViewController {
+
+    @IBAction private func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
